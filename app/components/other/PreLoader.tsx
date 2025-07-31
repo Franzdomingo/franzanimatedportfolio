d9399b924dd9e4eef3fd2be58b8ff0e14516d8c2
Franzdomingo <202110383@fit.edu.ph>
2025-07-31 16:06:18 +0800

    Update PreLoader component to reflect correct branding in loading message

--- app/components/other/PreLoader.tsx
+++ app/components/other/PreLoader.tsx
@@ -98,7 +98,7 @@ const PreLoader: React.FC = () => {
                 <span>By Franzdomingo.tech</span>
                 <span> / </span>
                
-                <span className="flex items-center justify-center gap-3"> Building  <Logo width={30} height={30} /> Eldora UI</span>
+                <span className="flex items-center justify-center gap-3"> Building  <Logo width={30} height={30} /> franzdomingo.tech</span>
                 <div className="sub hidden"></div>
             </div>
         </div>
